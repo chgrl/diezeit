4996a8f9f33cb3cc4a862accb3f78268a6c4dff6
chgrl <christian.graul@gmail.com>
2015-06-27 14:56:47 +0200

    more control over the search results

--- R/zeitr.R
+++ R/zeitr.R
@@ -1,5 +1,15 @@
-zeit_content <- function(query) {
-  req <- zeit_get(path="content", q=query)
+zeit_content <- function(query, fields=NA, limit=10, offset=0) {
+	# prepare query
+	if(length(query)==1) query <- gsub(" ", "%20", query, fixed=TRUE)
+	else query <- paste0(query, collapse="+")
+	
+	# check fields
+	avail.fields <- c("subtitle", "uuid", "title", "href", "release_date", "uri", "snippet", "supertitle", "teaser_text", "teaser_title")
+  if(!is.na(fields)) fields <- avail.fields[pmatch(fields, avail.fields)]
+	
+	# make request
+  if(is.na(fields)) req <- zeit_get(path="content", q=query, limit=limit, offset=offset)
+  req <- zeit_get(path="content", q=query, fields=fields, limit=limit, offset=offset)
   raw <- zeit_parse(req)
   invisible(raw)
 }
