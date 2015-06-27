7e58180443dda9d7537eb4bdd7b1a90d49677c86
chgrl <christian.graul@gmail.com>
2015-06-27 21:47:42 +0200

    fields fixed

--- R/zeitr.R
+++ R/zeitr.R
@@ -7,18 +7,18 @@ zeit_client <- function() {
 }
 
 
-zeit_content <- function(query, fields=NA, limit=10, offset=0) {
+zeit_content <- function(query, fields="all", limit=10, offset=0) {
 	# prepare query
-	if(length(query)==1) query <- gsub(" ", "%20", query, fixed=TRUE)
-	else query <- paste0(query, collapse="+")
-	
-	# check fields
+	query <- paste0(sapply(query, function(x) gsub(" ", "%20", x, fixed=TRUE)), collapse="+")
+	str(query)
+	# prepare fields
 	avail.fields <- c("subtitle", "uuid", "title", "href", "release_date", "uri", "snippet", "supertitle", "teaser_text", "teaser_title")
-  if(!is.na(fields)) fields <- avail.fields[pmatch(fields, avail.fields)]
+  if(fields!="all") fields <- avail.fields[pmatch(fields, avail.fields)]
+  if(length(fields)>1) fields <- paste0(fields, collapse=",")
 	
 	# make request
-  if(is.na(fields)) req <- zeit_get(path="content", q=query, limit=limit, offset=offset)
-  req <- zeit_get(path="content", q=query, fields=fields, limit=limit, offset=offset)
+  if(fields=="all") req <- zeit_get(path="content", q=query, limit=limit, offset=offset)
+  else req <- zeit_get(path="content", q=query, fields=fields, limit=limit, offset=offset)
   raw <- zeit_parse(req)
-  invisible(raw)
+  return(raw)
 }
