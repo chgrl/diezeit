95e9847a28c97cd425f32ee745b67fc62e80d7de
chgrl <christian.graul@gmail.com>
2015-07-05 21:07:11 +0200

    satisfy r-devel check

--- R/diezeit.R
+++ R/diezeit.R
@@ -1,5 +1,5 @@
 #' @title ZEIT ONLINE Content API
-#' @description \code{diezeit} is a wrapper for the ZEIT ONLINE Content API, 
+#' @description A wrapper for the ZEIT ONLINE Content API, 
 #' available at \url{http://developer.zeit.de}. It gives access to articles 
 #' and corresponding metadata from the ZEIT archive and from ZEIT ONLINE. 
 #' A personal API key is required for usage.
@@ -8,6 +8,7 @@
 #' @seealso \code{\link{zeit_client}} for client information and usage,
 #' \code{\link{zeit_search}} for ZEIT archive search.
 #' @importFrom httr GET
+#' @importFrom httr content
 #' @importFrom jsonlite fromJSON
 #' @aliases diezeit diezeit-package
 NULL
