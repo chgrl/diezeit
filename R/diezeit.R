6d328c4458d21b9f4c949beeffeafaf28e3516fa
chgrl <christian.graul@gmail.com>
2015-10-15 20:06:25 +0200

    details about API key added

--- R/diezeit.R
+++ R/diezeit.R
@@ -5,6 +5,8 @@
 #' A personal API key is required for usage.
 #' @name diezeit
 #' @docType package
+#' @details Accessing the ZEIT archive requires an API key, that can be requested at http://developer.zeit.de/quickstart. Registration is free and allows for API-Access with a limit of 10,000 requests per day. If you do not want to enter your key for each R session, put the following in your .Renviron or .Rprofile file:
+#' \code{ZEIT_KEY=PUTYOURKEYHERE}
 #' @seealso \code{\link{zeit_client}} for client information and usage,
 #' \code{\link{zeit_search}} for ZEIT archive search or \code{\link{zeit_get}} 
 #' to get content from the ZEIT archive.
